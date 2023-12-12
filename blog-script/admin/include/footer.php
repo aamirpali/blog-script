38279437ee63551e76326e6872f0abb3fba113cc
ameer baksh <aamir.pali@gmail.com>
2023-12-12 23:43:10 +0530

    Update footer.php

---
+++ blog-script/admin/include/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+        <div class="container">
+            Made with love ❤️ by <a href="https://ameerbaksh.cloud" target="_blank">Ameer Baksh</a> from India 🇮🇳
+        </div>
+    </div>
