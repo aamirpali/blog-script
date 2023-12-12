4332383964705c2deb9019c53717557322a3a708
ameer baksh <aamir.pali@gmail.com>
2023-12-12 23:40:15 +0530

    Update index.php

--- blog-script/admin/index.php
+++ blog-script/admin/index.php
@@ -1,61 +1,94 @@
 <?php
-require_once '../config.php';
+session_start(); // Start a PHP session
+require_once('../config.php'); // Include your database configuration
+require_once('include/header.php'); // Include your header
 
-if (isset($_POST['login'])) {
+// Check if the user is already logged in, redirect to dashboard.php
+if (isset($_SESSION['user_id'])) {
+    header("Location: dashboard.php");
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Check if the user exists
-    $sql = "SELECT password FROM admin WHERE username = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-    if ($result->num_rows === 1) {
-        $row = $result->fetch_assoc();
-        $hashedPassword = $row['password'];
-
-        if (password_verify($password, $hashedPassword)) {
-    // Successful login
-    session_start();
-    $_SESSION['admin_id'] = $adminId; // Set the admin_id session variable
-    $_SESSION['admin_username'] = $username;
-    header('Location: dashboard.php');
-    exit();
-} else {
-            $error_message = 'Invalid username or password';
-        }
-    } else {
-        $error_message = 'Invalid username or password';
+    if (!$connection) {
+        die("Database connection failed: " . mysqli_connect_error());
     }
+
+    $query = "SELECT * FROM admin WHERE username = ?";
+    $stmt = mysqli_prepare($connection, $query);
+
+    if ($stmt) {
+        mysqli_stmt_bind_param($stmt, "s", $username);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+
+        if ($result && mysqli_num_rows($result) === 1) {
+            $user = mysqli_fetch_assoc($result);
+            $hashedPassword = $user['password'];
+
+            if (password_verify($password, $hashedPassword)) {
+                // Password is correct
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['username'] = $user['username'];
+
+                // Your update logic here if needed
+
+                header("Location: dashboard.php"); // Redirect to the dashboard or another protected page
+                exit();
+            } else {
+                $loginError = "Invalid username or password.";
+            }
+        } else {
+            $loginError = "Invalid username or password.";
+        }
+
+        mysqli_stmt_close($stmt);
+    } else {
+        die("Prepared statement failed: " . mysqli_error($connection));
+    }
+
+    mysqli_close($connection);
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Admin Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Login</title>
+    <!-- Add your CSS and Bootstrap include here -->
 </head>
 <body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="login-container">
+                    <h1 class="form-heading">Login</h1>
+                    <?php if (isset($loginError)) { ?>
+                        <p class="error-message"><?php echo $loginError; ?></p>
+                    <?php } ?>
+                    <form method="POST">
+                        <div class="form-group">
+                            <label for="username">Username:</label>
+                            <input type="text" name="username" id="username" class="form-control" required>
+                        </div>
 
-<div class="container mt-5">
-    <h2>Admin Login</h2>
-    <?php if (isset($error_message)) { ?>
-        <div class="alert alert-danger"><?= $error_message ?></div>
-    <?php } ?>
-    <form method="post">
-        <div class="form-group">
-            <label for="username">Username:</label>
-            <input type="text" class="form-control" id="username" name="username" required>
-        </div>
-        <div class="form-group">
-            <label for="password">Password:</label>
-            <input type="password" class="form-control" id="password" name="password" required>
-        </div>
-        <button type="submit" class="btn btn-primary" name="login">Login</button>
-    </form>
-</div>
+                        <div class="form-group">
+                            <label for="password">Password:</label>
+                            <input type="password" name="password" id="password" class="form-control" required>
+                        </div>
 
+                        <div class="form-group">
+                            <input type="submit" value="Login" class="btn btn-primary btn-block">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
